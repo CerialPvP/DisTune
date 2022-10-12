a01587a3d8c9518dd892d608749235d460649dcb
CerialPvP <lolipopssbrawlstars@gmail.com>
2022-10-12 21:22:00 +0300

    🐛 Fixed bug which would crash the bot with no roles

--- src/events/guild/guildMemberAdd.js
+++ src/events/guild/guildMemberAdd.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require("discord.js")
+const { EmbedBuilder, Embed } = require("discord.js")
 const { QuickDB } = require("quick.db")
 const db = new QuickDB({ filePath: "./database/autorole.sqlite" })
 
@@ -7,17 +7,27 @@ module.exports = {
     async execute(member) {
         const { guild } = member
 
+        const getLogChannel = await db.get(`${guild.id}_logchannel`)
+        const logchannel = guild.channels.cache.get(getLogChannel)
         const roles = await db.get(`${guild.id}_roles`)
         const roleObj = []
         for (const role in roles) {
             var pushedObj = guild.roles.cache.get(roles[role])
+            if (!pushedObj) {
+                db.delete(`${guild.id}_logchannel`)
+                db.delete(`${guild.id}_roles`)
+                if (logchannel) {
+                    const errEmbed = new EmbedBuilder()
+                        .setColor("Red").setTitle("Autorole - System Deleted")
+                        .setDescription("The role which is set in the autorole has been deleted.\nBecause of that, AutoRole has been removed from your server.\nYou can set it up again anytime with `/autorole setup`.")
+                    return logchannel.send({embeds: [errEmbed]})
+                }
+            }
             roleObj.push(pushedObj)
         }
 
         await member.roles.add(roleObj)
 
-        const getLogChannel = await db.get(`${guild.id}_logchannel`)
-        const logchannel = guild.channels.cache.get(getLogChannel)
         if (logchannel) {
             const embed = new EmbedBuilder()
                 .setColor("Yellow").setTitle("New member joined!")
